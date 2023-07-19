48143321d9ad5563ad81fbe32c621c8fe5c39898
Abderraouf1985 <abderraouftlili@gmail.com>
2023-07-19 01:24:03 -0400

    Ajout de l'application

---
+++ WebApplication30/WebApplication30/Controllers/ItemsController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication30.Data;
+using WebApplication30.Models;
+
+namespace WebApplication30.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+
+
+
+
+        public ItemsController(AppDbContext db)
+        {
+            _db = db;
+        }
+        
+        
+        
+        
+        private readonly AppDbContext _db;
+
+
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemsList = _db.Items.ToList();
+            return View(itemsList);
+        }
+        //GET
+        public IActionResult New() 
+        {
+        return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Item item)
+        {
+            if (item.Name == "100") 
+            {
+                ModelState.AddModelError("Name", "NAME CANNOT BE EQUAL 100");
+            
+            
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Items.Add(item);
+                _db.SaveChanges();
+                TempData["successData"] = "Item has been added succesfully";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(item);
+
+            }
+
+
+
+        }
+
+
+        //GET
+        public IActionResult Edit(int? Id)
+        {
+
+            if (Id== null || Id==0 )
+                    
+            {
+
+                return NotFound();     
+
+            }
+            var item = _db.Items.Find(Id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Item item)
+        { 
+            if (item.Name == "100")
+            {
+                ModelState.AddModelError("Name", "NAME CANNOT BE EQUAL 100");
+
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Items.Update(item);
+                _db.SaveChanges();
+                TempData["successData"] = "Item has been updated succesfully";
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View(item);
+
+            }
+
+
+
+        }
+
+
+        //GET
+        public IActionResult Delete(int? Id)
+        {
+
+            if (Id == null || Id == 0)
+
+            {
+
+                return NotFound();
+
+            }
+            var item = _db.Items.Find(Id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteItem(int? Id)
+        {
+           var item = _db.Items.Find(Id);
+            if (item == null) 
+            {
+                return NotFound();
+            }
+            _db.Remove(item);    
+            _db.SaveChanges();
+            TempData["successData"] = "Item has been deleted succesfully";
+            return RedirectToAction("Index");
+
+            
+         
+
+
+        }
+
+    }
+}
---
+++ WebApplication30/WebApplication30/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication30.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        [Required]
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+    }
+}
